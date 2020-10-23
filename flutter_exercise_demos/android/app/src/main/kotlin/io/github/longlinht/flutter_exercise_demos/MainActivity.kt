985c910492b092baeb8391912b0fa21a52c25c55
hetao <hetaoof@gmail.com>
2020-10-23 16:13:42 +0800

    add round corner bottom sheet demo;

---
+++ flutter_exercise_demos/android/app/src/main/kotlin/io/github/longlinht/flutter_exercise_demos/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.longlinht.flutter_exercise_demos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_exercise_demos/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'ui/MyHomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exercise Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Exercise Demo'),
+    );
+  }
+}
+
